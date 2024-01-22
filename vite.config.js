e5439ffdb157c2bbd3184837b8b7c7f40e56fba7
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-01-22 09:16:51 +0100

    adding

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import ViteSassPlugin from "@vitejs/plugin-sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ViteSassPlugin()],
+});
