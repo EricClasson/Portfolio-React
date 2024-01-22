98ee920623059ef4f2b433949032c885efb62bbd
Eric Classon <142800136+EricClasson@users.noreply.github.com>
2024-01-22 09:20:31 +0100

    addingnr2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ViteSassPlugin from "@vitejs/plugin-sass";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteSassPlugin()],
+  plugins: [react()],
 });
